20ddb3a5d1c1a74f22db76243be148da2d042e41
Dylan Dsouza <dsouzadyn@gmail.com>
2024-02-17 21:42:05 +0530

    complete single node broadcast

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use uuid::Uuid;
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub enum MaelstromMessageBodyType {
@@ -15,6 +16,18 @@ pub enum MaelstromMessageBodyType {
     Generate,
     #[serde(rename = "generate_ok")]
     GenerateOK,
+    #[serde(rename = "broadcast")]
+    Broadcast,
+    #[serde(rename = "broadcast_ok")]
+    BroadcastOK,
+    #[serde(rename = "read")]
+    Read,
+    #[serde(rename = "read_ok")]
+    ReadOK,
+    #[serde(rename = "topology")]
+    Topology,
+    #[serde(rename = "topology_ok")]
+    TopologyOK,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,6 +42,12 @@ pub struct MaelstromMessageBody {
     echo: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     id: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    message: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    messages: Option<Vec<i32>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    topology: Option<HashMap<String, Vec<String>>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -43,7 +62,7 @@ impl MaelstromMessage {
         serde_json::from_str::<MaelstromMessage>(&message).unwrap()
     }
 
-    pub fn eval(&mut self) {
+    pub fn eval(&mut self, memory: &mut Vec<i32>) {
         // Swap source and destinations
         let new_src = self.dest.clone();
         let new_dest = self.src.clone();
@@ -65,6 +84,22 @@ impl MaelstromMessage {
                 self.body.in_reply_to = self.body.msg_id;
                 self.body.id = Option::from(Uuid::new_v4().to_string());
             }
+            MaelstromMessageBodyType::Broadcast => {
+                self.body.body_type = MaelstromMessageBodyType::BroadcastOK;
+                self.body.in_reply_to = self.body.msg_id;
+                memory.push(self.body.message.unwrap());
+                self.body.message = Option::None;
+            }
+            MaelstromMessageBodyType::Read => {
+                self.body.body_type = MaelstromMessageBodyType::ReadOK;
+                self.body.in_reply_to = self.body.msg_id;
+                self.body.messages = Option::from(memory.clone());
+            }
+            MaelstromMessageBodyType::Topology => {
+                self.body.body_type = MaelstromMessageBodyType::TopologyOK;
+                self.body.in_reply_to = self.body.msg_id;
+                self.body.topology = Option::None;
+            }
             _ => eprintln!("unkown message body type"),
         }
     }
@@ -85,7 +120,7 @@ mod tests {
     fn can_respond_to_init_message() {
         let message = r#"{"src": "c1","dest": "n1","body": {"type": "init","msg_id": 1}}"#;
         let mut maelstrom_message: MaelstromMessage = MaelstromMessage::parse(message);
-        maelstrom_message.eval();
+        maelstrom_message.eval(&mut Vec::new());
         assert_eq!(maelstrom_message.dest, "c1");
         assert_eq!(maelstrom_message.src, "n1");
         assert_eq!(maelstrom_message.body.in_reply_to, Option::from(1));
@@ -98,7 +133,7 @@ mod tests {
     fn can_respond_to_echo_message() {
         let message = r#"{"src": "c1","dest": "n1","body": {"type": "echo","msg_id": 1,"echo": "Please echo 35"}}"#;
         let mut maelstrom_message: MaelstromMessage = MaelstromMessage::parse(message);
-        maelstrom_message.eval();
+        maelstrom_message.eval(&mut Vec::new());
         assert_eq!(maelstrom_message.dest, "c1");
         assert_eq!(maelstrom_message.src, "n1");
         assert_eq!(maelstrom_message.body.in_reply_to, Option::from(1));
@@ -111,7 +146,7 @@ mod tests {
     fn can_respond_to_generate_message() {
         let message = r#"{"src": "c1","dest": "n1","body": {"type": "generate","msg_id": 1}}"#;
         let mut maelstrom_message: MaelstromMessage = MaelstromMessage::parse(message);
-        maelstrom_message.eval();
+        maelstrom_message.eval(&mut Vec::new());
         assert_eq!(maelstrom_message.dest, "c1");
         assert_eq!(maelstrom_message.src, "n1");
         assert_eq!(maelstrom_message.body.in_reply_to, Option::from(1));
@@ -119,5 +154,52 @@ mod tests {
             maelstrom_message.body.body_type,
             MaelstromMessageBodyType::GenerateOK
         )
+    }
+    #[test]
+    fn can_respond_to_broadcast_messag() {
+        let message = r#"{"src": "c1","dest": "n1","body": {"type": "broadcast","msg_id": 1, "message": -1}}"#;
+        let mut maelstrom_message: MaelstromMessage = MaelstromMessage::parse(message);
+        maelstrom_message.eval(&mut Vec::new());
+        assert_eq!(maelstrom_message.dest, "c1");
+        assert_eq!(maelstrom_message.src, "n1");
+        assert_eq!(maelstrom_message.body.in_reply_to, Option::from(1));
+        assert_eq!(
+            maelstrom_message.body.body_type,
+            MaelstromMessageBodyType::BroadcastOK
+        )
+    }
+    #[test]
+    fn can_respond_to_read_message() {
+        let message1 = r#"{"src": "c1","dest": "n1","body": {"type": "broadcast","msg_id": 1, "message": -1}}"#;
+        let message2 =
+            r#"{"src": "c1","dest": "n1","body": {"type": "broadcast","msg_id": 2, "message": 0}}"#;
+        let message3 = r#"{"src": "c1","dest": "n1","body": {"type": "read","msg_id": 3}}"#;
+        let mut memory: Vec<i32> = Vec::new();
+        let mut maelstrom_message: MaelstromMessage = MaelstromMessage::parse(message1);
+        maelstrom_message.eval(&mut memory);
+        maelstrom_message = MaelstromMessage::parse(message2);
+        maelstrom_message.eval(&mut memory);
+        maelstrom_message = MaelstromMessage::parse(message3);
+        maelstrom_message.eval(&mut memory);
+        assert_eq!(maelstrom_message.dest, "c1");
+        assert_eq!(maelstrom_message.src, "n1");
+        assert_eq!(maelstrom_message.body.in_reply_to, Option::from(3));
+        assert_eq!(
+            maelstrom_message.body.messages,
+            Option::from(Vec::from([-1, 0]))
+        );
+        assert_eq!(
+            maelstrom_message.body.body_type,
+            MaelstromMessageBodyType::ReadOK
+        )
+    }
+    #[test]
+    fn can_respond_to_topology_message() {
+        let message = r#"{"src": "c1","dest": "n1","body": {"type": "topology","msg_id": 1, "topology": {"n1": ["n2", "n3"]}}}"#;
+        let mut maelstrom_message: MaelstromMessage = MaelstromMessage::parse(message);
+        maelstrom_message.eval(&mut Vec::new());
+        assert_eq!(maelstrom_message.dest, "c1");
+        assert_eq!(maelstrom_message.src, "n1");
+        assert_eq!(maelstrom_message.body.in_reply_to, Option::from(1));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use maelstrom::MaelstromMessage;
 use std::io;
 fn main() -> io::Result<()> {
+    let mut memory: Vec<i32> = Vec::new();
     loop {
         let mut input = String::new();
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
                 let trimmed_input = input.trim();
                 let mut message = MaelstromMessage::parse(trimmed_input);
-                message.eval();
+                message.eval(&mut memory);
                 println!("{message}");
             }
             Err(error) => {
